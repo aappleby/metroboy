48c47775d0c09aecf6ade6aba0da7d40c6d7bef4
Austin Appleby <aappleby@gmail.com>
2019-04-23 20:23:07 -0700

    slightly simpler oam int tests

--- microtests/oam_int_if_d.s
+++ microtests/oam_int_if_d.s
@@ -1,23 +1,17 @@
-; pass - ags
+; pass - ags, dmg
 
 .include "header.inc"
 
-; 95 - off
-; 96 - on
-; 99 - on
-; 100 - off
-
 main:
   di
-
   lcd_off_unsafe
   lcd_on
+  nops 114
 
-  nops 100
-
-  set_ie_stat
-  clear_if
   set_stat_int_oam
+  nops 104
 
+  clear_if
   ldh a, (IF)
+
   test_finish_a $E0

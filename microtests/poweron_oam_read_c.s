2c0bc4f47757493a48cc63ebc1b7b67bb5dd5df3
Austin Appleby <aappleby@gmail.com>
2019-10-07 21:37:20 -0700

    need to fix oam read

---
+++ microtests/poweron_oam_read_c.s
@@ -0,0 +1,18 @@
+.include "header.inc"
+
+.define DELAY  113
+.define ADDR   $FE00
+.define RESULT $F0
+
+//  63 = FF
+//  64 = F0
+// 113 = F0
+// 114 = FF
+
+main:
+  ld a, RESULT
+  ld (ADDR), a
+  nops DELAY
+  ld a, (ADDR)
+
+  test_finish_a RESULT

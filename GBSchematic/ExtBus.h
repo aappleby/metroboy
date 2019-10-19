792b0b63e78155aeab6ca41eadac98326786322a
Austin Appleby <aappleby@gmail.com>
2019-10-19 13:45:55 -0700

    checkpoitn

--- GBSchematic/ExtBus.h
+++ GBSchematic/ExtBus.h
@@ -32,6 +32,20 @@ struct ExtBus {
 
 extern ExtBus ext;
 
+extern bool MCS_A, MCS_D, MCS_IN;
+extern bool MOE_A, MOE_D, MOE_IN;
+extern bool MWR_A, MWR_D, MWR_IN;
+
+// chip-to-vram data bus
+extern bool MD0_A, MD0_OUT, MD0_IN;
+extern bool MD1_A, MD1_OUT, MD1_IN;
+extern bool MD2_A, MD2_OUT, MD2_IN;
+extern bool MD3_A, MD3_OUT, MD3_IN;
+extern bool MD4_A, MD4_OUT, MD4_IN;
+extern bool MD5_A, MD5_OUT, MD5_IN;
+extern bool MD6_A, MD6_OUT, MD6_IN;
+extern bool MD7_A, MD7_OUT, MD7_IN;
+
 // package RD/WR
 extern bool WR_A, WR_C, WR_IN; // is this supposed to be the input of the /WR gpio?
 extern bool RD_A, RD_B, RD_C;  // what is the B for?
--- GBSchematic/VramBus.h
+++ GBSchematic/VramBus.h
@@ -27,17 +27,3 @@ struct Vram {
 
 extern Vram vram;
 
-extern bool MCS_A, MCS_D, MCS_IN;
-extern bool MOE_A, MOE_D, MOE_IN;
-extern bool MWR_A, MWR_D, MWR_IN;
-
-// chip-to-vram data bus
-extern bool MD0_A, MD0_OUT, MD0_IN;
-extern bool MD1_A, MD1_OUT, MD1_IN;
-extern bool MD2_A, MD2_OUT, MD2_IN;
-extern bool MD3_A, MD3_OUT, MD3_IN;
-extern bool MD4_A, MD4_OUT, MD4_IN;
-extern bool MD5_A, MD5_OUT, MD5_IN;
-extern bool MD6_A, MD6_OUT, MD6_IN;
-extern bool MD7_A, MD7_OUT, MD7_IN;
-

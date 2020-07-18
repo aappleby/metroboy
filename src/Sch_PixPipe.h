bed72df25787be80295548e95c4c94c5015f97db
Austin Appleby <aappleby@gmail.com>
2020-07-17 21:24:26 -0700

    checkpoint

--- src/Sch_PixPipe.h
+++ src/Sch_PixPipe.h
@@ -148,59 +148,59 @@ private:
   Reg LCD_PIN_LD1;  // PIN_50 
   Reg LCD_PIN_LD0;  // PIN_51 
 
-  /*p32.MYDE*/ Reg BG_PIPE_A0;
-  /*p32.NOZO*/ Reg BG_PIPE_A1;
-  /*p32.MOJU*/ Reg BG_PIPE_A2;
-  /*p32.MACU*/ Reg BG_PIPE_A3;
-  /*p32.NEPO*/ Reg BG_PIPE_A4;
-  /*p32.MODU*/ Reg BG_PIPE_A5;
-  /*p32.NEDA*/ Reg BG_PIPE_A6;
-  /*p32.PYBO*/ Reg BG_PIPE_A7;
+  /*p32.MYDE*/ Reg2 BG_PIPE_A0 = Reg2::D0C0;
+  /*p32.NOZO*/ Reg2 BG_PIPE_A1 = Reg2::D0C0;
+  /*p32.MOJU*/ Reg2 BG_PIPE_A2 = Reg2::D0C0;
+  /*p32.MACU*/ Reg2 BG_PIPE_A3 = Reg2::D0C0;
+  /*p32.NEPO*/ Reg2 BG_PIPE_A4 = Reg2::D0C0;
+  /*p32.MODU*/ Reg2 BG_PIPE_A5 = Reg2::D0C0;
+  /*p32.NEDA*/ Reg2 BG_PIPE_A6 = Reg2::D0C0;
+  /*p32.PYBO*/ Reg2 BG_PIPE_A7 = Reg2::D0C0;
 
-  /*p32.TOMY*/ Reg BG_PIPE_B0;
-  /*p32.TACA*/ Reg BG_PIPE_B1;
-  /*p32.SADY*/ Reg BG_PIPE_B2;
-  /*p32.RYSA*/ Reg BG_PIPE_B3;
-  /*p32.SOBO*/ Reg BG_PIPE_B4;
-  /*p32.SETU*/ Reg BG_PIPE_B5;
-  /*p32.RALU*/ Reg BG_PIPE_B6;
-  /*p32.SOHU*/ Reg BG_PIPE_B7;
+  /*p32.TOMY*/ Reg2 BG_PIPE_B0 = Reg2::D0C0;
+  /*p32.TACA*/ Reg2 BG_PIPE_B1 = Reg2::D0C0;
+  /*p32.SADY*/ Reg2 BG_PIPE_B2 = Reg2::D0C0;
+  /*p32.RYSA*/ Reg2 BG_PIPE_B3 = Reg2::D0C0;
+  /*p32.SOBO*/ Reg2 BG_PIPE_B4 = Reg2::D0C0;
+  /*p32.SETU*/ Reg2 BG_PIPE_B5 = Reg2::D0C0;
+  /*p32.RALU*/ Reg2 BG_PIPE_B6 = Reg2::D0C0;
+  /*p32.SOHU*/ Reg2 BG_PIPE_B7 = Reg2::D0C0;
 
-  /*p33.NURO*/ Reg SPR_PIPE_B0;
-  /*p33.MASO*/ Reg SPR_PIPE_B1;
-  /*p33.LEFE*/ Reg SPR_PIPE_B2;
-  /*p33.LESU*/ Reg SPR_PIPE_B3;
-  /*p33.WYHO*/ Reg SPR_PIPE_B4;
-  /*p33.WORA*/ Reg SPR_PIPE_B5;
-  /*p33.VAFO*/ Reg SPR_PIPE_B6;
-  /*p33.WUFY*/ Reg SPR_PIPE_B7;
+  /*p33.NURO*/ Reg2 SPR_PIPE_B0 = Reg2::D0C0;
+  /*p33.MASO*/ Reg2 SPR_PIPE_B1 = Reg2::D0C0;
+  /*p33.LEFE*/ Reg2 SPR_PIPE_B2 = Reg2::D0C0;
+  /*p33.LESU*/ Reg2 SPR_PIPE_B3 = Reg2::D0C0;
+  /*p33.WYHO*/ Reg2 SPR_PIPE_B4 = Reg2::D0C0;
+  /*p33.WORA*/ Reg2 SPR_PIPE_B5 = Reg2::D0C0;
+  /*p33.VAFO*/ Reg2 SPR_PIPE_B6 = Reg2::D0C0;
+  /*p33.WUFY*/ Reg2 SPR_PIPE_B7 = Reg2::D0C0;
 
-  /*p33.NYLU*/ Reg SPR_PIPE_A0;
-  /*p33.PEFU*/ Reg SPR_PIPE_A1;
-  /*p33.NATY*/ Reg SPR_PIPE_A2;
-  /*p33.PYJO*/ Reg SPR_PIPE_A3;
-  /*p33.VARE*/ Reg SPR_PIPE_A4;
-  /*p33.WEBA*/ Reg SPR_PIPE_A5;
-  /*p33.VANU*/ Reg SPR_PIPE_A6;
-  /*p33.VUPY*/ Reg SPR_PIPE_A7;
+  /*p33.NYLU*/ Reg2 SPR_PIPE_A0 = Reg2::D0C0;
+  /*p33.PEFU*/ Reg2 SPR_PIPE_A1 = Reg2::D0C0;
+  /*p33.NATY*/ Reg2 SPR_PIPE_A2 = Reg2::D0C0;
+  /*p33.PYJO*/ Reg2 SPR_PIPE_A3 = Reg2::D0C0;
+  /*p33.VARE*/ Reg2 SPR_PIPE_A4 = Reg2::D0C0;
+  /*p33.WEBA*/ Reg2 SPR_PIPE_A5 = Reg2::D0C0;
+  /*p33.VANU*/ Reg2 SPR_PIPE_A6 = Reg2::D0C0;
+  /*p33.VUPY*/ Reg2 SPR_PIPE_A7 = Reg2::D0C0;
 
-  /*p34.RUGO*/ Reg PAL_PIPE_0;
-  /*p34.SATA*/ Reg PAL_PIPE_1;
-  /*p34.ROSA*/ Reg PAL_PIPE_2;
-  /*p34.SOMY*/ Reg PAL_PIPE_3;
-  /*p34.PALU*/ Reg PAL_PIPE_4;
-  /*p34.NUKE*/ Reg PAL_PIPE_5;
-  /*p34.MODA*/ Reg PAL_PIPE_6;
-  /*p34.LYME*/ Reg PAL_PIPE_7;
+  /*p34.RUGO*/ Reg2 PAL_PIPE_0 = Reg2::D0C0;
+  /*p34.SATA*/ Reg2 PAL_PIPE_1 = Reg2::D0C0;
+  /*p34.ROSA*/ Reg2 PAL_PIPE_2 = Reg2::D0C0;
+  /*p34.SOMY*/ Reg2 PAL_PIPE_3 = Reg2::D0C0;
+  /*p34.PALU*/ Reg2 PAL_PIPE_4 = Reg2::D0C0;
+  /*p34.NUKE*/ Reg2 PAL_PIPE_5 = Reg2::D0C0;
+  /*p34.MODA*/ Reg2 PAL_PIPE_6 = Reg2::D0C0;
+  /*p34.LYME*/ Reg2 PAL_PIPE_7 = Reg2::D0C0;
 
-  /*p26.VEZO*/ Reg MASK_PIPE_0;
-  /*p26.WURU*/ Reg MASK_PIPE_1;
-  /*p26.VOSA*/ Reg MASK_PIPE_2;
-  /*p26.WYFU*/ Reg MASK_PIPE_3;
-  /*p26.XETE*/ Reg MASK_PIPE_4;
-  /*p26.WODA*/ Reg MASK_PIPE_5;
-  /*p26.VUMO*/ Reg MASK_PIPE_6;
-  /*p26.VAVA*/ Reg MASK_PIPE_7;
+  /*p26.VEZO*/ Reg2 MASK_PIPE_0 = Reg2::D0C0;
+  /*p26.WURU*/ Reg2 MASK_PIPE_1 = Reg2::D0C0;
+  /*p26.VOSA*/ Reg2 MASK_PIPE_2 = Reg2::D0C0;
+  /*p26.WYFU*/ Reg2 MASK_PIPE_3 = Reg2::D0C0;
+  /*p26.XETE*/ Reg2 MASK_PIPE_4 = Reg2::D0C0;
+  /*p26.WODA*/ Reg2 MASK_PIPE_5 = Reg2::D0C0;
+  /*p26.VUMO*/ Reg2 MASK_PIPE_6 = Reg2::D0C0;
+  /*p26.VAVA*/ Reg2 MASK_PIPE_7 = Reg2::D0C0;
 
   // FF47 - BGP
   /*p36.PAVO*/ Reg2 _PAVO_BGP0 = Reg2::D0C0;

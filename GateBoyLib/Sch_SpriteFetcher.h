886d7e9f407f5d75bdaca808b40c7f9de3a02bc2
Austin Appleby <aappleby@gmail.com>
2020-10-06 23:12:29 -0700

    checkpoint

--- GateBoyLib/Sch_SpriteFetcher.h
+++ GateBoyLib/Sch_SpriteFetcher.h
@@ -10,36 +10,60 @@ struct VramBus;
 
 struct SpriteFetcher {
   void reset() {}
-  void reset_bootrom() {}
+
+  void reset_bootrom() {
+    WUTY_SPRITE_DONEp.reset(TRI_HZNP);
+    WEFY_SPR_READp.reset(TRI_HZNP);
+    VAPE_FETCH_OAM_CLK.reset(TRI_HZNP);
+    SOHO_SPR_VRAM_RDp.reset(TRI_HZNP);
+    TEXY_SPR_READ_VRAMp.reset(TRI_HZNP);
+    ABON_SPR_VRM_RDn.reset(TRI_HZNP);
+    XUQU_SPRITE_AB.reset(TRI_HZNP);
+    RACA_LATCH_SPPIXB.reset(TRI_HZNP);
+    TOPU_LATCH_SPPIXA.reset(TRI_HZNP);
+
+    TAKA_SFETCH_RUNNINGp.reset(TRI_D0NP);
+    SOBU_SFETCH_REQp.reset(REG_D0C0);
+    SUDA_SFETCH_REQp.reset(REG_D0C0);
+
+    TOXE_SFETCH_S0.reset(REG_D0C0);
+    TYFO_SFETCH_S0_D1.reset(REG_D0C0);
+    TULY_SFETCH_S1.reset(REG_D0C0);
+    TESE_SFETCH_S2.reset(REG_D0C0);
+    TOBU_SFETCH_S1_D2.reset(REG_D0C0);
+    VONU_SFETCH_S1_D4.reset(REG_D0C0);
+    SEBA_SFETCH_S1_D5.reset(REG_D0C0);
+  }
+
   void tick(const SchematicTop& gb);
   void tock(SchematicTop& gb);
   void dump(Dumper& d) const;
 
-  /* p29.WUTY*/ Sig WUTY_SPRITE_DONEp;   // -> pix pipe, sprite store
-  /* p28.WEFY*/ Sig WEFY_SPR_READp;      // -> oam bus
-  /* p25.VAPE*/ Sig VAPE_FETCH_OAM_CLK;  // -> oam bus
-  /* p25.SOHO*/ Sig SOHO_SPR_VRAM_RDp;   // -> vram bus
-  /* p29.TEXY*/ Sig TEXY_SPR_READ_VRAMp; // -> vram bus
-  /* p29.ABON*/ Sig ABON_SPR_VRM_RDn;    // -> vram bus
-  /*#p29.XUQU*/ Sig XUQU_SPRITE_AB;      // -> vram bus
-  /*#p29.RACA*/ Sig RACA_LATCH_SPPIXB;   // -> vram bus
-  /*#p29.TOPU*/ Sig TOPU_LATCH_SPPIXA;   // -> vram bus
+  /* p29.WUTY*/ Sig WUTY_SPRITE_DONEp = ERR_XXXX;   // -> pix pipe, sprite store
+  /* p28.WEFY*/ Sig WEFY_SPR_READp = ERR_XXXX;      // -> oam bus
+  /* p25.VAPE*/ Sig VAPE_FETCH_OAM_CLK = ERR_XXXX;  // -> oam bus
+  /* p25.SOHO*/ Sig SOHO_SPR_VRAM_RDp = ERR_XXXX;   // -> vram bus
+  /* p29.TEXY*/ Sig TEXY_SPR_READ_VRAMp = ERR_XXXX; // -> vram bus
+  /* p29.ABON*/ Sig ABON_SPR_VRM_RDn = ERR_XXXX;    // -> vram bus
+  /*#p29.XUQU*/ Sig XUQU_SPRITE_AB = ERR_XXXX;      // -> vram bus
+  /*#p29.RACA*/ Sig RACA_LATCH_SPPIXB = ERR_XXXX;   // -> vram bus
+  /*#p29.TOPU*/ Sig TOPU_LATCH_SPPIXA = ERR_XXXX;   // -> vram bus
 
 private:
 
   // When TOXE & TYFO & VONU & SEBA, sprites are loaded into the pipe.
 
-  /*p27.TAKA*/ NandLatch TAKA_SFETCH_RUNNINGp;
-  /*p27.SOBU*/ DFF17 SOBU_SFETCH_REQp;
-  /*p27.SUDA*/ DFF17 SUDA_SFETCH_REQp;
+  /*p27.TAKA*/ NandLatch TAKA_SFETCH_RUNNINGp = ERR_XXXX;
+  /*p27.SOBU*/ DFF17 SOBU_SFETCH_REQp = ERR_XXXX;
+  /*p27.SUDA*/ DFF17 SUDA_SFETCH_REQp = ERR_XXXX;
 
-  /*p29.TOXE*/ DFF17 TOXE_SFETCH_S0;
-  /*p29.TYFO*/ DFF17 TYFO_SFETCH_S0_D1;
-  /*p29.TULY*/ DFF17 TULY_SFETCH_S1;
-  /*p29.TESE*/ DFF17 TESE_SFETCH_S2;
-  /*p29.TOBU*/ DFF17 TOBU_SFETCH_S1_D2;
-  /*p29.VONU*/ DFF17 VONU_SFETCH_S1_D4;
-  /*p29.SEBA*/ DFF17 SEBA_SFETCH_S1_D5;
+  /*p29.TOXE*/ DFF17 TOXE_SFETCH_S0 = ERR_XXXX;
+  /*p29.TYFO*/ DFF17 TYFO_SFETCH_S0_D1 = ERR_XXXX;
+  /*p29.TULY*/ DFF17 TULY_SFETCH_S1 = ERR_XXXX;
+  /*p29.TESE*/ DFF17 TESE_SFETCH_S2 = ERR_XXXX;
+  /*p29.TOBU*/ DFF17 TOBU_SFETCH_S1_D2 = ERR_XXXX;
+  /*p29.VONU*/ DFF17 VONU_SFETCH_S1_D4 = ERR_XXXX;
+  /*p29.SEBA*/ DFF17 SEBA_SFETCH_S1_D5 = ERR_XXXX;
 };
 
 //-----------------------------------------------------------------------------

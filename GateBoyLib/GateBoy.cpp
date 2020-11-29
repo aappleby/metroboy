ac92edc6729ced8cf24c61e96a6c79dc90b71b9c
Austin Appleby <aappleby@gmail.com>
2020-11-28 18:34:41 -0800

    checkpoint

--- GateBoyLib/GateBoy.cpp
+++ GateBoyLib/GateBoy.cpp
@@ -1215,51 +1215,10 @@ void GateBoy::tock_slow() {
   /* p04.LARA*/ dma_reg.LARA_DMA_LATCHn = nand3(dma_reg.LOKY_DMA_LATCHp.qp(), dma_reg.MYTE_DMA_DONE.qn(), _CUNU_SYS_RSTn_s);
   /*#p04.LOKY*/ dma_reg.LOKY_DMA_LATCHp = nand2(dma_reg.LARA_DMA_LATCHn.qp(), dma_reg.LENE_DMA_TRIG_d4.qn());
 
-  /*#p04.LUVY*/ dma_reg.LUVY_DMA_TRIG_d0 .dff17(_UVYT_ABCDxxxx_s, _CUNU_SYS_RSTn_s, _LUPA_DMA_TRIG);
-  /*#p04.LENE*/ dma_reg.LENE_DMA_TRIG_d4 .dff17(_MOPA_xxxxEFGH, _CUNU_SYS_RSTn_s, dma_reg.LUVY_DMA_TRIG_d0.qp());
-
-
-  /*#p04.MYTE*/ dma_reg.MYTE_DMA_DONE.dff17(_MOPA_xxxxEFGH, _LAPA_DMA_RSTn, _NOLO_DMA_DONEp);
-  /*#p04.MATU*/ dma_reg.MATU_DMA_RUNNINGp.dff17(_UVYT_ABCDxxxx_s, _CUNU_SYS_RSTn_s, dma_reg.LOKY_DMA_LATCHp.qp());
-
-  /*#p04.NAKY*/ dma_reg.NAKY_DMA_A00p.dff17(_META_DMA_CLKp,               _LAPA_DMA_RSTn, dma_reg.NAKY_DMA_A00p.qn());
-  /*#p04.PYRO*/ dma_reg.PYRO_DMA_A01p.dff17(dma_reg.NAKY_DMA_A00p.qn(), _LAPA_DMA_RSTn, dma_reg.PYRO_DMA_A01p.qn());
-  /* p04.NEFY*/ dma_reg.NEFY_DMA_A02p.dff17(dma_reg.PYRO_DMA_A01p.qn(), _LAPA_DMA_RSTn, dma_reg.NEFY_DMA_A02p.qn());
-  /* p04.MUTY*/ dma_reg.MUTY_DMA_A03p.dff17(dma_reg.NEFY_DMA_A02p.qn(), _LAPA_DMA_RSTn, dma_reg.MUTY_DMA_A03p.qn());
-  /* p04.NYKO*/ dma_reg.NYKO_DMA_A04p.dff17(dma_reg.MUTY_DMA_A03p.qn(), _LAPA_DMA_RSTn, dma_reg.NYKO_DMA_A04p.qn());
-  /* p04.PYLO*/ dma_reg.PYLO_DMA_A05p.dff17(dma_reg.NYKO_DMA_A04p.qn(), _LAPA_DMA_RSTn, dma_reg.PYLO_DMA_A05p.qn());
-  /* p04.NUTO*/ dma_reg.NUTO_DMA_A06p.dff17(dma_reg.PYLO_DMA_A05p.qn(), _LAPA_DMA_RSTn, dma_reg.NUTO_DMA_A06p.qn());
-  /* p04.MUGU*/ dma_reg.MUGU_DMA_A07p.dff17(dma_reg.NUTO_DMA_A06p.qn(), _LAPA_DMA_RSTn, dma_reg.MUGU_DMA_A07p.qn());
-
-  /*#p04.NAFA*/ dma_reg.NAFA_DMA_A08n.dff8p(_LORU_FF46_WRn, BUS_CPU_Dp_in_s[0].qp());
-  /* p04.PYNE*/ dma_reg.PYNE_DMA_A09n.dff8p(_LORU_FF46_WRn, BUS_CPU_Dp_in_s[1].qp());
-  /* p04.PARA*/ dma_reg.PARA_DMA_A10n.dff8p(_LORU_FF46_WRn, BUS_CPU_Dp_in_s[2].qp());
-  /* p04.NYDO*/ dma_reg.NYDO_DMA_A11n.dff8p(_LORU_FF46_WRn, BUS_CPU_Dp_in_s[3].qp());
-  /* p04.NYGY*/ dma_reg.NYGY_DMA_A12n.dff8p(_LORU_FF46_WRn, BUS_CPU_Dp_in_s[4].qp());
-  /* p04.PULA*/ dma_reg.PULA_DMA_A13n.dff8p(_LORU_FF46_WRn, BUS_CPU_Dp_in_s[5].qp());
-  /* p04.POKU*/ dma_reg.POKU_DMA_A14n.dff8p(_LORU_FF46_WRn, BUS_CPU_Dp_in_s[6].qp());
-  /* p04.MARU*/ dma_reg.MARU_DMA_A15n.dff8p(_LORU_FF46_WRn, BUS_CPU_Dp_in_s[7].qp());
-
-  /*#p04.LOKO*/ _LOKO_DMA_RSTp = nand2(dma_reg.LENE_DMA_TRIG_d4.qn(), _CUNU_SYS_RSTn_s);
-  /*#p04.LUPA*/ _LUPA_DMA_TRIG = nor2(_LAVY_FF46_WRp_s, dma_reg.LYXE_DMA_LATCHp.qn());
-  /*#p04.META*/ _META_DMA_CLKp = and2(_UVYT_ABCDxxxx_s, dma_reg.LOKY_DMA_LATCHp.qp());
-  /*#p04.LAPA*/ _LAPA_DMA_RSTn = not1(_LOKO_DMA_RSTp);
-
-  /*#p04.NAVO*/ _NAVO_DMA_DONEn = nand6(dma_reg.NAKY_DMA_A00p.qp(), dma_reg.PYRO_DMA_A01p.qp(),  // 128+16+8+4+2+1 = 159
-                                         dma_reg.NEFY_DMA_A02p.qp(), dma_reg.MUTY_DMA_A03p.qp(),
-                                         dma_reg.NYKO_DMA_A04p.qp(), dma_reg.MUGU_DMA_A07p.qp());
-  /*#p04.NOLO*/ _NOLO_DMA_DONEp = not1(_NAVO_DMA_DONEn);
 
   /*#p04.LYXE*/ dma_reg.LYXE_DMA_LATCHp.nor_latch(_LAVY_FF46_WRp_s, _LOKO_DMA_RSTp);
   /* p04.LARA*/ dma_reg.LARA_DMA_LATCHn = nand3(dma_reg.LOKY_DMA_LATCHp.qp(), dma_reg.MYTE_DMA_DONE.qn(), _CUNU_SYS_RSTn_s);
   /*#p04.LOKY*/ dma_reg.LOKY_DMA_LATCHp = nand2(dma_reg.LARA_DMA_LATCHn.qp(), dma_reg.LENE_DMA_TRIG_d4.qn());
-
-  /*#p04.LUVY*/ dma_reg.LUVY_DMA_TRIG_d0 .dff17(_UVYT_ABCDxxxx_s, _CUNU_SYS_RSTn_s, _LUPA_DMA_TRIG);
-  /*#p04.LENE*/ dma_reg.LENE_DMA_TRIG_d4 .dff17(_MOPA_xxxxEFGH, _CUNU_SYS_RSTn_s, dma_reg.LUVY_DMA_TRIG_d0.qp());
-
-
-  /*#p04.MYTE*/ dma_reg.MYTE_DMA_DONE.dff17(_MOPA_xxxxEFGH, _LAPA_DMA_RSTn, _NOLO_DMA_DONEp);
-  /*#p04.MATU*/ dma_reg.MATU_DMA_RUNNINGp.dff17(_UVYT_ABCDxxxx_s, _CUNU_SYS_RSTn_s, dma_reg.LOKY_DMA_LATCHp.qp());
 
   //================================================================================
 

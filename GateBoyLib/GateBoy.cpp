31b098a011c2604fa42add2c27ecf109e42370c0
Austin Appleby <aappleby@gmail.com>
2021-01-20 11:13:08 -0800

    test test

--- GateBoyLib/GateBoy.cpp
+++ GateBoyLib/GateBoy.cpp
@@ -615,6 +615,7 @@ void GateBoy::tock_slow(int pass_index) {
   /*#p24.SEGU*/ wire SEGU_CLKPIPE_evn = not1(TYFA_CLKPIPE_odd);
   /*#p24.ROXO*/ wire ROXO_CLKPIPE_odd = not1(SEGU_CLKPIPE_evn);
 
+
   // vvvvvvvvvv
   {
     tock_sprite_fetcher(TEKY_SFETCH_REQp_old);

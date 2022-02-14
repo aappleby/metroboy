f0a9c25f0eac0925923faaf5cf36707fa0b1f344
Austin Appleby <aappleby@gmail.com>
2022-02-14 01:47:03 -0800

    checkpoint

--- src/Metron/Metron.cpp
+++ src/Metron/Metron.cpp
@@ -6,29 +6,7 @@
 #include "../uart_test/ibex_pkg.h"
 #include "../uart_test/ibex_alu.h"
 
-#include "../../riscv-simple-sv/synth/config.h"
-#include "../../riscv-simple-sv/core/common/constants.h"
-#include "../../riscv-simple-sv/core/common/adder.h"
-#include "../../riscv-simple-sv/core/common/alu.h"
-#include "../../riscv-simple-sv/core/common/alu_control.h"
-#include "../../riscv-simple-sv/core/common/control_transfer.h"
-#include "../../riscv-simple-sv/core/common/data_memory_interface.h"
-#include "../../riscv-simple-sv/core/common/immediate_generator.h"
-#include "../../riscv-simple-sv/core/common/instruction_decoder.h"
-#include "../../riscv-simple-sv/core/common/multiplexer.h"
-#include "../../riscv-simple-sv/core/common/multiplexer2.h"
-#include "../../riscv-simple-sv/core/common/multiplexer4.h"
-#include "../../riscv-simple-sv/core/common/multiplexer8.h"
-#include "../../riscv-simple-sv/core/common/regfile.h"
-#include "../../riscv-simple-sv/core/common/register.h"
-#include "../../riscv-simple-sv/core/common/example_text_memory.h"
-#include "../../riscv-simple-sv/core/common/example_text_memory_bus.h"
-#include "../../riscv-simple-sv/core/singlecycle/riscv_core.h"
-#include "../../riscv-simple-sv/core/singlecycle/singlecycle_control.h"
-#include "../../riscv-simple-sv/core/singlecycle/singlecycle_ctlpath.h"
-#include "../../riscv-simple-sv/core/singlecycle/singlecycle_datapath.h"
-
-multiplexer<> blah_multiplexer;
+#include "../../riscv-simple-sv/core/singlecycle/toplevel.h"
 
 //------------------------------------------------------------------------------
 

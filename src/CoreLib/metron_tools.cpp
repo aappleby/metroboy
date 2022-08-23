6f1ae5b3f6a6ad3c5ba07b2a8dc7fbc2343c06cd
Austin Appleby <aappleby@gmail.com>
2022-08-23 01:13:08 -0700

    accio logico

---
+++ src/CoreLib/metron_tools.cpp
@@ -0,0 +1,1 @@
+#include "metron_tools.h"
--- src/MetroBoyLib/MetroBoySPU2.h
+++ src/MetroBoyLib/MetroBoySPU2.h
@@ -1,34 +1,27 @@
 #pragma once
 #include "CoreLib/Types.h"
 
+#include "CoreLib/metron_tools.h"
+
 //-----------------------------------------------------------------------------
 
 class MetroBoySPU2 {
 public:
 
-  static int b1(int x, int o) { return (x >> o) & ((1 << 1) - 1); }
-  static int b2(int x, int o) { return (x >> o) & ((1 << 2) - 1); }
-  static int b3(int x, int o) { return (x >> o) & ((1 << 3) - 1); }
-  static int b4(int x, int o) { return (x >> o) & ((1 << 4) - 1); }
-  static int b5(int x, int o) { return (x >> o) & ((1 << 5) - 1); }
-  static int b6(int x, int o) { return (x >> o) & ((1 << 6) - 1); }
-  static int b7(int x, int o) { return (x >> o) & ((1 << 7) - 1); }
-  static int b8(int x, int o) { return (x >> o) & ((1 << 8) - 1); }
+  logic<9> out_r;
+  logic<9> out_l;
 
-  uint16_t out_r;
-  uint16_t out_l;
+  logic<8> ack_data;
+  logic<1> ack_valid;
 
-  uint8_t ack_data;
-  bool ack_valid;
-
-  bool posedge_bit(int t_old, int t_new, int b) {
-    return posedge(bit(t_old, b), bit(t_new, b));
+  logic<1> posedge_bit(int t_old, int t_new, int b) {
+    return posedge(b1(t_old, b), b1(t_new, b));
   }
 
   //----------------------------------------
 
   void tock_out() {
-    uint8_t s3_volume_shift = 0;
+    logic<3> s3_volume_shift = 0;
     switch (b2(nr32, 5)) {
       case 0: s3_volume_shift = 4; break;
       case 1: s3_volume_shift = 0; break;
@@ -39,16 +32,16 @@ public:
     //----------
     // output
 
-    int s1_out = 0;
-    int s2_out = 0;
-    int s3_out = 0;
-    int s4_out = 0;
+    logic<4> s1_out = 0;
+    logic<4> s2_out = 0;
+    logic<4> s3_out = 0;
+    logic<4> s4_out = 0;
 
-    auto s1_duty = b2(nr11, 6);
-    auto s2_duty = b2(nr21, 6);
+    logic<2> s1_duty = b2(nr11, 6);
+    logic<2> s2_duty = b2(nr21, 6);
 
-    auto s3_power = b1(nr30, 7);
-    auto s3_vol   = b2(nr32, 5);
+    logic<1> s3_power = b1(nr30, 7);
+    logic<2> s3_vol   = b2(nr32, 5);
 
     if (s1_running) {
       switch(s1_duty) {
@@ -57,7 +50,7 @@ public:
         case 2: s1_out = b1(0b11100001, s1_phase); break;
         case 3: s1_out = b1(0b01111110, s1_phase); break;
       }
-      s1_out *= s1_env_vol;
+      s1_out = s1_out * s1_env_vol;
     }
 
     if (s2_running) {
@@ -67,24 +60,23 @@ public:
         case 2: s2_out = b1(0b11100001, s2_phase); break;
         case 3: s2_out = b1(0b01111110, s2_phase); break;
       }
-      s2_out *= s2_env_vol;
+      s2_out = s2_out * s2_env_vol;
     }
 
     if (s3_running && s3_power) {
-      uint8_t s3_sample = s3_wave[s3_phase >> 1];
+      logic<8> s3_sample = s3_wave[s3_phase >> 1];
       s3_out = (s3_phase & 1) ? b4(s3_sample, 0) : b4(s3_sample, 4);
 
       switch (s3_vol) {
-      case 0: s3_out >>= 4;  break;
-      case 1: s3_out >>= 0; break;
-      case 2: s3_out >>= 1;  break;
-      case 3: s3_out >>= 2;  break;
+      case 0: s3_out = s3_out >> 4;  break;
+      case 1: s3_out = s3_out >> 0; break;
+      case 2: s3_out = s3_out >> 1;  break;
+      case 3: s3_out = s3_out >> 2;  break;
       }
     }
 
     if (s4_running) {
-      //s4_out = b1(s4_lfsr, 15) * s4_env_vol;
-      s4_out = b1(s4_lfsr, 0) * s4_env_vol;
+      s4_out = b1(s4_lfsr, 15) * s4_env_vol;
     }
 
     //----------
@@ -102,8 +94,8 @@ public:
     if (bit(nr51, 6)) out_l = out_l + s3_out;
     if (bit(nr51, 7)) out_l = out_l + s4_out;
 
-    uint8_t volume_r = b3(nr50, 0) + 1;
-    uint8_t volume_l = b3(nr50, 4) + 1;
+    logic<4> volume_r = b3(nr50, 0) + 1;
+    logic<4> volume_l = b3(nr50, 4) + 1;
 
     out_r = out_r * volume_r;
     out_l = out_l * volume_l;
@@ -181,57 +173,57 @@ public:
 
   //----------------------------------------
 
-  void tick(bool reset, uint16_t addr, uint8_t data, bool we) {
+  void tick(logic<1> reset, logic<16> addr, logic<8> data, logic<1> we) {
     if (reset) {
       tick_reset();
       return;
     }
 
-    auto spu_clock_old = spu_clock;
-    auto spu_clock_new = (spu_clock + 1) & 0x3FFF;
+    logic<16> spu_clock_old = spu_clock;
+    logic<16> spu_clock_new = (spu_clock + 1) & 0x3FFF;
     
-    bool sweep_tick =  (spu_clock_new & 0b01111111111111) == 0b01000000000000;
-    bool length_tick = (spu_clock_new & 0b00111111111111) == 0b00000000000000;
-    bool env_tick =    (spu_clock_new & 0b11111111111111) == 0b11100000000000;
+    logic<1> sweep_tick =  (spu_clock_new & 0b01111111111111) == 0b01000000000000;
+    logic<1> length_tick = (spu_clock_new & 0b00111111111111) == 0b00000000000000;
+    logic<1> env_tick =    (spu_clock_new & 0b11111111111111) == 0b11100000000000;
 
-    auto s1_trig = b1(nr14, 7);
-    auto s2_trig = b1(nr24, 7);
-    auto s3_trig = b1(nr34, 7);
-    auto s4_trig = b1(nr44, 7);
+    logic<1> s1_trig = b1(nr14, 7);
+    logic<1> s2_trig = b1(nr24, 7);
+    logic<1> s3_trig = b1(nr34, 7);
+    logic<1> s4_trig = b1(nr44, 7);
 
-    auto s1_len_timer_init = b6(nr11, 0);
-    auto s2_len_timer_init = b6(nr21, 0);
-    auto s3_len_timer_init = b8(nr31, 0);
-    auto s4_len_timer_init = b6(nr41, 0);
+    logic<6> s1_len_timer_init = b6(nr11, 0);
+    logic<6> s2_len_timer_init = b6(nr21, 0);
+    logic<8> s3_len_timer_init = b8(nr31, 0);
+    logic<6> s4_len_timer_init = b6(nr41, 0);
 
-    auto s1_len_en = b1(nr14, 6);
-    auto s2_len_en = b1(nr24, 6);
-    auto s3_len_en = b1(nr34, 6);
-    auto s4_len_en = b1(nr44, 6);
+    logic<1> s1_len_en = b1(nr14, 6);
+    logic<1> s2_len_en = b1(nr24, 6);
+    logic<1> s3_len_en = b1(nr34, 6);
+    logic<1> s4_len_en = b1(nr44, 6);
 
-    auto s1_env_timer_init = b3(nr12, 0);
-    auto s2_env_timer_init = b3(nr22, 0);
-    auto s4_env_timer_init = b3(nr42, 0);
+    logic<3> s1_env_timer_init = b3(nr12, 0);
+    logic<3> s2_env_timer_init = b3(nr22, 0);
+    logic<3> s4_env_timer_init = b3(nr42, 0);
 
-    auto s1_env_add = b1(nr12, 3);
-    auto s2_env_add = b1(nr22, 3);
-    auto s4_env_add = b1(nr42, 3);
+    logic<1> s1_env_add = b1(nr12, 3);
+    logic<1> s2_env_add = b1(nr22, 3);
+    logic<1> s4_env_add = b1(nr42, 3);
 
-    auto s1_env_vol_init = b4(nr12, 4);
-    auto s2_env_vol_init = b4(nr22, 4);
-    auto s4_env_vol_init = b4(nr42, 4);
+    logic<4> s1_env_vol_init = b4(nr12, 4);
+    logic<4> s2_env_vol_init = b4(nr22, 4);
+    logic<4> s4_env_vol_init = b4(nr42, 4);
 
-    auto s1_sweep_shift  = b3(nr10, 0);
-    auto s1_sweep_dir    = b1(nr10, 3);
-    auto s1_sweep_timer_init = b3(nr10, 4);
+    logic<3> s1_sweep_shift  = b3(nr10, 0);
+    logic<1> s1_sweep_dir    = b1(nr10, 3);
+    logic<3> s1_sweep_timer_init = b3(nr10, 4);
     
-    auto s1_freq_timer_init = (b3(nr14, 0) << 8) | nr13;
-    auto s2_freq_timer_init = (b3(nr24, 0) << 8) | nr23;
-    auto s3_freq_timer_init = (b3(nr34, 0) << 8) | nr33;
-    auto s4_freq_timer_init = b3(nr43, 0);
+    logic<11> s1_freq_timer_init = (b3(nr14, 0) << 8) | nr13;
+    logic<11> s2_freq_timer_init = (b3(nr24, 0) << 8) | nr23;
+    logic<11> s3_freq_timer_init = (b3(nr34, 0) << 8) | nr33;
+    logic<3>  s4_freq_timer_init = b3(nr43, 0);
 
-    auto s4_mode  = b1(nr43, 3);
-    auto s4_shift = b4(nr43, 4);
+    logic<1> s4_mode  = b1(nr43, 3);
+    logic<4> s4_shift = b4(nr43, 4);
 
     //----------
     // s1 clock
@@ -267,27 +259,17 @@ public:
 
     if (posedge_bit(spu_clock_old, spu_clock_new, s4_shift + 1)) {
       if (s4_freq_timer) {
-        s4_freq_timer--;
+        s4_freq_timer = s4_freq_timer - 1;
       }
       else {
-        bool new_bit = b1(s4_lfsr, 0) ^ b1(s4_lfsr, 1);
-        s4_lfsr >>= 1;
+        logic<1> new_bit = b1(s4_lfsr, 15) ^ b1(s4_lfsr, 14);
+        s4_lfsr = s4_lfsr << 1;
 
-        auto mask = s4_mode ? 0b0100000001000000 : 0b0100000000000000;
+        logic<16> mask = s4_mode ? 0b0000001000000010: 0b0000000000000010;
 
-        s4_lfsr &= ~mask;
-        s4_lfsr |= new_bit ? mask : 0;
-
-        /*
-        auto new_bit = bit(s4_lfsr, 14) ^ bit(s4_lfsr, 15);
-        s4_lfsr <<= 1;
-        if (s4_mode) {
-          s4_lfsr = (s4_lfsr & 0xFEFF) | (new_bit << 8);
-        }
-        else {
-          s4_lfsr |= new_bit ? 0b00000001 : 0b00000000;
-        }
-        */
+        s4_lfsr = s4_lfsr & ~mask;
+        s4_lfsr = s4_lfsr | (new_bit ? mask : b16(0));
+        
         s4_freq_timer = s4_freq_timer_init;
       }
     }
@@ -300,7 +282,7 @@ public:
         s1_sweep_timer = s1_sweep_timer - 1;
       }
       else {
-        uint16_t delta = s1_sweep_freq >> s1_sweep_shift;
+        logic<16> delta = s1_sweep_freq >> s1_sweep_shift;
         s1_sweep_freq = s1_sweep_freq + (s1_sweep_dir ? -delta : +delta);
         if (s1_sweep_freq > 2047) s1_running = 0;
         s1_sweep_timer = s1_sweep_timer_init;
@@ -312,31 +294,40 @@ public:
 
     if (length_tick) {
       if (s1_running && s1_len_en) {
-        s1_len_timer++;
-        if (s1_len_timer == 64) {
+        if (s1_len_timer == 63) {
           s1_len_timer = 0;
           s1_running = 0;
+          
+        }
+        else {
+          s1_len_timer = s1_len_timer + 1;
         }
       }
       if (s2_running && s2_len_en) {
-        s2_len_timer++;
-        if (s2_len_timer == 64) {
+        if (s2_len_timer == 63) {
           s2_len_timer = 0;
           s2_running = 0;
         }
+        else {
+          s2_len_timer = s2_len_timer + 1;
+        }
       }
       if (s3_running && s3_len_en) {
-        s3_len_timer++;
-        if (s3_len_timer == 256) {
+        if (s3_len_timer == 255) {
           s3_len_timer = 0;
           s3_running = 0;
         }
+        else {
+          s3_len_timer = s3_len_timer + 1;
+        }
       }
       if (s4_running && s4_len_en) {
-        s4_len_timer++;
-        if (s4_len_timer == 64) {
+        if (s4_len_timer == 63) {
           s4_len_timer = 0;
           s4_running = 0;
+        }
+        else {
+          s4_len_timer = s4_len_timer + 1;
         }
       }
     }
@@ -351,8 +342,8 @@ public:
         }
         else {
           s1_env_timer = s1_env_timer_init;
-          if (s1_env_add) { if (s1_env_vol < 15) s1_env_vol++; }
-          else            { if (s1_env_vol >  0) s1_env_vol--; }
+          if (s1_env_add) { if (s1_env_vol < 15) s1_env_vol = s1_env_vol + 1; }
+          else            { if (s1_env_vol >  0) s1_env_vol = s1_env_vol - 1; }
         }
       }
 
@@ -362,8 +353,8 @@ public:
         }
         else {
           s2_env_timer = s2_env_timer_init;
-          if (s2_env_add) { if (s2_env_vol < 15) s2_env_vol++; }
-          else            { if (s2_env_vol >  0) s2_env_vol--; }
+          if (s2_env_add) { if (s2_env_vol < 15) s2_env_vol = s2_env_vol + 1; }
+          else            { if (s2_env_vol >  0) s2_env_vol = s2_env_vol - 1; }
         }
       }
 
@@ -373,8 +364,8 @@ public:
         }
         else {
           s4_env_timer = s4_env_timer_init;
-          if (s4_env_add) { if (s4_env_vol < 15) s4_env_vol++; }
-          else            { if (s4_env_vol >  0) s4_env_vol--; }
+          if (s4_env_add) { if (s4_env_vol < 15) s4_env_vol = s4_env_vol + 1; }
+          else            { if (s4_env_vol >  0) s4_env_vol = s4_env_vol - 1; }
         }
       }
     }
@@ -391,10 +382,10 @@ public:
     //----------
     // Triggers
 
-    bool s1_trigger = we && addr == 0xFF14 && (data & 0x80);
-    bool s2_trigger = we && addr == 0xFF19 && (data & 0x80);
-    bool s3_trigger = we && addr == 0xFF1E && (data & 0x80);
-    bool s4_trigger = we && addr == 0xFF23 && (data & 0x80);
+    logic<1> s1_trigger = we && addr == 0xFF14 && (data & 0x80);
+    logic<1> s2_trigger = we && addr == 0xFF19 && (data & 0x80);
+    logic<1> s3_trigger = we && addr == 0xFF1E && (data & 0x80);
+    logic<1> s4_trigger = we && addr == 0xFF23 && (data & 0x80);
 
     if (s1_trigger && (s1_env_vol_init || s1_env_add)) {
       s1_running     = 1;
@@ -456,22 +447,22 @@ public:
 
         case 0xFF20: {
           nr41 = data | 0b11000000;
-          printf("nr41 0x%02x len 64 - %d\n", nr41, b6(nr41, 0));
+          //printf("nr41 0x%02x len 64 - %d\n", nr41, b6(nr41, 0));
           break;
         }
         case 0xFF21: {
           nr42 = data | 0b00000000;
-          printf("nr42 0x%02x env period %d add %d init %d\n", nr42, b3(nr42, 0), b1(nr42, 3), b4(nr42, 4));
+          //printf("nr42 0x%02x env period %d add %d init %d\n", nr42, b3(nr42, 0), b1(nr42, 3), b4(nr42, 4));
           break;
         }
         case 0xFF22: {
           nr43 = data | 0b00000000;
-          printf("nr43 0x%02x lfsr delay %d width %d clock mux %d\n", nr43, b3(nr43, 0), b1(nr43, 3), b4(nr43, 4));
+          //printf("nr43 0x%02x lfsr delay %d width %d clock mux %d\n", nr43, b3(nr43, 0), b1(nr43, 3), b4(nr43, 4));
           break;
         }
         case 0xFF23: {
           nr44 = data | 0b00111111;
-          printf("nr44 0x%02x len en %d trig %d\n", nr44, b1(nr44, 6), b1(nr44, 7));
+          //printf("nr44 0x%02x len en %d trig %d\n", nr44, b1(nr44, 6), b1(nr44, 7));
           break;
         }
 
@@ -513,7 +504,7 @@ public:
 
     const char* bar = "===============";
 
-    uint8_t s3_env_vol = 0;
+    logic<4> s3_env_vol = 0;
     switch (b2(nr32, 5)) {
     case 0: s3_env_vol = 0; break;
     case 1: s3_env_vol = 15; break;
@@ -556,8 +547,8 @@ public:
 
     char buf[33];
     for (int i = 0; i < 16; i++) {
-      uint8_t a = b4(s3_wave[i], 4);
-      uint8_t b = b4(s3_wave[i], 0);
+      logic<4> a = b4(s3_wave[i], 4);
+      logic<4> b = b4(s3_wave[i], 0);
 
       buf[2 * i + 0] = a > 9 ? 'A' + a - 10 : '0' + a;
       buf[2 * i + 1] = b > 9 ? 'B' + b - 10 : '0' + b;
@@ -569,67 +560,66 @@ public:
 
 private:
 
-  uint8_t nr10;
-  uint8_t nr11;
-  uint8_t nr12;
-  uint8_t nr13;
+  logic<8> nr10;
+  logic<8> nr11;
+  logic<8> nr12;
+  logic<8> nr13;
+  logic<8> nr14;
 
-  uint8_t nr14;
-  uint8_t nr20;
-  uint8_t nr21;
-  uint8_t nr22;
+  logic<8> nr20;
+  logic<8> nr21;
+  logic<8> nr22;
+  logic<8> nr23;
+  logic<8> nr24;
 
-  uint8_t nr23;
-  uint8_t nr24;
-  uint8_t nr30;
-  uint8_t nr31;
+  logic<8> nr30;
+  logic<8> nr31;
+  logic<8> nr32;
+  logic<8> nr33;
+  logic<8> nr34;
 
-  uint8_t nr32;
-  uint8_t nr33;
-  uint8_t nr34;
-  uint8_t nr40;
+  logic<8> nr40;
+  logic<8> nr41;
+  logic<8> nr42;
+  logic<8> nr43;
+  logic<8> nr44;
 
-  uint8_t nr41;
-  uint8_t nr42;
-  uint8_t nr43;
-  uint8_t nr44;
+  logic<8> nr50;
+  logic<8> nr51;
+  logic<8> nr52;
 
-  uint8_t nr50;
-  uint8_t nr51;
-  uint8_t nr52;
+  logic<16> spu_clock;
 
-  uint16_t spu_clock;
+  logic<1> s1_running;
+  logic<1> s2_running;
+  logic<1> s3_running;
+  logic<1> s4_running;
 
-  bool s1_running;
-  bool s2_running;
-  bool s3_running;
-  bool s4_running;
+  logic<6> s1_len_timer;
+  logic<6> s2_len_timer;
+  logic<8> s3_len_timer;
+  logic<6> s4_len_timer;
 
-  uint8_t  s1_len_timer;
-  uint8_t  s2_len_timer;
-  uint16_t s3_len_timer;
-  uint8_t  s4_len_timer;
+  logic<3> s1_env_timer;
+  logic<3> s2_env_timer;
+  logic<3> s4_env_timer;
 
-  uint8_t  s1_env_timer;
-  uint8_t  s2_env_timer;
-  uint8_t  s4_env_timer;
+  logic<4> s1_env_vol;
+  logic<4> s2_env_vol;
+  logic<4> s4_env_vol;
 
-  uint8_t  s1_env_vol;
-  uint8_t  s2_env_vol;
-  uint8_t  s4_env_vol;
+  logic<3>  s1_sweep_timer;
+  logic<11> s1_sweep_freq;
 
-  uint8_t  s1_sweep_timer;
-  uint16_t s1_sweep_freq;
+  logic<11> s1_freq_timer;
+  logic<11> s2_freq_timer;
+  logic<11> s3_freq_timer;
+  logic<3>  s4_freq_timer;
 
-  uint16_t s1_freq_timer;
-  uint16_t s2_freq_timer;
-  uint16_t s3_freq_timer;
-  uint16_t s4_freq_timer;
+  logic<3>  s1_phase;
+  logic<3>  s2_phase;
+  logic<5>  s3_phase;
 
-  uint8_t  s1_phase;
-  uint8_t  s2_phase;
-  uint8_t  s3_phase;
-
-  uint8_t  s3_wave[16];
-  uint16_t s4_lfsr;
+  logic<8>  s3_wave[16];
+  logic<16> s4_lfsr;
 };

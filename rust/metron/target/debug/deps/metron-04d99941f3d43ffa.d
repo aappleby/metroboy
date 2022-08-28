f372b63e27907610528e7496f59a6d3a00b7baf6
Austin Appleby <aappleby@TinyTron.localdomain>
2022-08-27 19:44:21 -0700

    why was rust/metron in there?

--- rust/metron/target/debug/deps/metron-04d99941f3d43ffa.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/aappl/Desktop/MetroBoy/rust/metron/target/debug/deps/metron-04d99941f3d43ffa: src/main.rs
-
-/mnt/c/Users/aappl/Desktop/MetroBoy/rust/metron/target/debug/deps/metron-04d99941f3d43ffa.d: src/main.rs
-
-src/main.rs:
--- rust/metron/target/debug/metron.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/aappl/Desktop/MetroBoy/rust/metron/target/debug/metron: /mnt/c/Users/aappl/Desktop/MetroBoy/rust/metron/src/main.rs
